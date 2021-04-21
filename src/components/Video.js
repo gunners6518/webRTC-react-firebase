63a21ce19901ee2c5dd69f33a910f4d3798d5716
gunners6518 <gunners6518@gmail.com>
2021-04-21 17:40:40 +0900

    videoコンポーネントにmaterial-uiのcardを導入

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
 
 export const Video = ({ isLocal, name, videoRef }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <video ref={videoRef} autoPlay muted={isLocal} />
-      <div>{name}</div>
-    </div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <video ref={videoRef} autoPlay muted={isLocal} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions></CardActions>
+    </Card>
   );
 };
